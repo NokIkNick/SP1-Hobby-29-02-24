d19134ba74bedfa3e3478db1f92b9f8cf5387691
Sir Rolin <nicolaieno@msn.com>
2024-02-26 17:51:05 +0100

    Hobby Config

---
+++ sp1.hobby/src/main/java/cphbusiness/groupone/config/HobbyConfig.java
@@ -0,0 +1,19 @@
+package cphbusiness.groupone.config;
+
+import cphbusiness.groupone.model.*;
+import jakarta.persistence.EntityManagerFactory;
+
+import java.util.Set;
+
+public class HobbyConfig {
+    private static EntityManagerFactory emf;
+    private static Set<Class> classes = Set.of(Address.class, Gender.class, Hobby.class, User.class, UserDetails.class, Zip.class);
+    private static boolean isDevEnv = false;
+    public static EntityManagerFactory getInstance(boolean devEnv){
+        if((emf == null || !emf.isOpen()) || devEnv != isDevEnv){
+            emf = HibernateConfig.getEntityManagerFactoryConfig(devEnv, classes);
+            isDevEnv = devEnv;
+        }
+        return emf;
+    }
+}
