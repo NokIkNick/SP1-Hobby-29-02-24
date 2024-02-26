05ea46097da257a79079a9188260122f03088bf6
Nicklas W <Nicklas2002@hotmail.com>
2024-02-26 17:06:25 +0100

    Started implementing abstract DAO

--- sp1.hobby/src/main/java/cphbusiness/groupone/dao/IDAO.java
+++ sp1.hobby/src/main/java/cphbusiness/groupone/dao/IDAO.java
@@ -1,5 +1,11 @@
 package cphbusiness.groupone.dao;
 
 public interface IDAO<T>{
+    void create(T in);
 
+    T read(int id);
+
+    T update(T obj,int id);
+
+    void delete(int id);
 }
--- sp1.hobby/src/main/java/cphbusiness/groupone/dao/abstractDAOs/DAO.java
+++ sp1.hobby/src/main/java/cphbusiness/groupone/dao/abstractDAOs/DAO.java
@@ -1,6 +1,43 @@
 package cphbusiness.groupone.dao.abstractDAOs;
 
+import cphbusiness.groupone.config.HibernateConfig;
 import cphbusiness.groupone.dao.IDAO;
+import jakarta.persistence.EntityManagerFactory;
+import org.hibernate.Hibernate;
 
 public abstract class DAO <T> implements IDAO<T> {
+
+
+    private static EntityManagerFactory emf;
+
+    private EntityManagerFactory getInstance(EntityManagerFactory emf_){
+        if(emf == null){
+            emf = emf_;
+        }
+        return emf;
+    }
+
+
+    public DAO(EntityManagerFactory emf_){
+        emf = getInstance(emf_);
+    }
+
+
+    public void create(T in){
+        try(var em = emf.createEntityManager()){
+            em.getTransaction().begin();
+            if(in != null){
+                em.persist(in);
+                em.getTransaction().commit();
+            }
+        }
+    }
+
+    public T read (int id, T in){
+        try(var em = emf.createEntityManager()){
+            Object found = (T) em.find(in.getClass(), id);
+            if(obj != null)
+        }
+    }
+
 }
