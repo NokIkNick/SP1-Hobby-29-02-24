3cb0707c78e661a2f0bcfac12bdf556bc7f036d8
Odiegenx <124783324+Odiegenx@users.noreply.github.com>
2024-02-27 16:52:45 +0100

    Worked on US 5

--- sp1.hobby/src/main/java/cphbusiness/groupone/dao/implementations/HobbyDAOImpl.java
+++ sp1.hobby/src/main/java/cphbusiness/groupone/dao/implementations/HobbyDAOImpl.java
@@ -2,6 +2,8 @@ package cphbusiness.groupone.dao.implementations;
 
 import cphbusiness.groupone.dao.abstractDAOs.HobbyDAO;
 import cphbusiness.groupone.model.Hobby;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
 
 public class HobbyDAOImpl extends HobbyDAO {
 
@@ -18,4 +20,17 @@ public class HobbyDAOImpl extends HobbyDAO {
         super();
     }
 
+    @Override
+    public int getCountOfPeopleByHobbyId(int id) {
+        try(EntityManager em = super.emf.createEntityManager()){
+            Query amoutOfPeopleByHobby = em.createNamedQuery("Hobby.countOfPeopleByHobby")
+                    .setParameter(1,id);
+            if(amoutOfPeopleByHobby != null) {
+                return (int) amoutOfPeopleByHobby.getSingleResult();
+            }else {
+                // logic to handle nothing found
+                return 0;
+            }
+        }
+    }
 }
