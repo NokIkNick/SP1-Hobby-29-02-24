590d235283c80a507e78fa49e1fb815ccd68f2e8
chris4567890 <starchristian4@hotmail.com>
2024-02-27 13:15:37 +0100

    made the thing I think
    was just supposed to get the city and the zip not region etc.

--- sp1.hobby/src/main/java/cphbusiness/groupone/Main.java
+++ sp1.hobby/src/main/java/cphbusiness/groupone/Main.java
@@ -14,28 +14,9 @@ import java.util.Set;
 public class Main {
     public static void main(String[] args) {
         EntityManagerFactory emf = HobbyConfig.getInstance(false);
-        UserDAOImpl userDAO = UserDAOImpl.getInstance();
-        HobbyDAOImpl hobbyDAO = HobbyDAOImpl.getInstance();
-        AddressDAOImpl addressDAO = AddressDAOImpl.getInstance();
-        ZipDAOImpl zipDAO = ZipDAOImpl.getInstance();
-
-
-        Hobby hobby1 = hobbyDAO.read(1,Hobby.class);
-        Zip zip1 = zipDAO.read(9293, Zip.class);
-
-        User user1 = new User("Christian1234","1234",false);
-        Address address1 = new Address("Lyngby Hovedgade 2");
-        UserDetails user1Details = new UserDetails();
-        user1Details.setAge(24);
-        user1Details.setGender(Gender.MALE);
-        user1Details.setPhone_number(4558879);
-
-        address1.setZip(zip1);
-        user1Details.addAddress(address1);
-
-        user1.addUserDetails(user1Details);
-        user1.addHobby(hobby1);
-
-        userDAO.create(user1);
+        ZipDAOImpl zipDAO = new ZipDAOImpl();
+        System.out.println(zipDAO.listOfCitiesAndZip());
     }
+
+
 }
